81cbd1b724e3a6825bfda3edc2d244499bdea5a4
Christopher Mühl <github@padarom.io>
2017-09-22 17:24:01 +0200

    Export guacamole module

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var insert = require('gulp-insert');
 
 gulp.task('default', function() {
     return gulp.src('guacamole-common-js/src/main/webapp/modules/*.js')
         .pipe(concat('guacamole-common.js'))
+        .pipe(insert.append('module.exports = Guacamole;'))
         .pipe(gulp.dest('dist'))
         .pipe(uglify())
         .pipe(rename('guacamole-common.min.js'))
